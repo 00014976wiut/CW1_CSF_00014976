fc7b0b059461f24071af4e416697170daf15e0fe
00014976wiut <117806834+00014976wiut@users.noreply.github.com>
2022-11-17 23:27:46 +0500

    Update CW1_0014976.py

--- CW1_0014976.py
+++ CW1_0014976.py
@@ -51,3 +51,30 @@ for i in range(presents_size):
     id2 = canvas.create_oval(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=present_color1)    
     presents_list.append([x, y, id1, id2])
 print(presents_list)
+
+#Creating Snake
+def snake_paint_item(canvas, x, y):
+    global snake_list
+    id1 = canvas.create_rectangle(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=snake_color2)
+    id2 = canvas.create_rectangle(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=snake_color1)
+    snake_list.append([x,y,id1,id2])
+    #print(snake_list)
+
+snake_paint_item(canvas, snake_x, snake_y)
+
+def check_can_we_delete_snake_item():
+    if len(snake_list) >= snake_size:
+        temp_item = snake_list.pop(0)
+        #print(temp_item)
+        canvas.delete(temp_item[2])
+        canvas.delete(temp_item[3])
+
+def check_if_we_found_present():
+    global snake_size
+    for i in range(len(presents_list)):
+        if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
+            #print("found!!!")
+            snake_size = snake_size + 1
+            canvas.delete(presents_list[i][2])
+            canvas.delete(presents_list[i][3])
+    #print(snake_x, snake_y)
