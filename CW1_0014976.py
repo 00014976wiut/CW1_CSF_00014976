dd23692c93290005f915b1b7fa52ee7483c9631d
00014976wiut <117806834+00014976wiut@users.noreply.github.com>
2022-11-17 23:25:57 +0500

    Update CW1_0014976.py

--- CW1_0014976.py
+++ CW1_0014976.py
@@ -27,3 +27,27 @@ snake_y_nav = 0
 
 snake_list = []
 snake_size = 5
+
+tk = Tk()
+tk.title("Hungry Snake")
+tk.resizable(0,0)
+
+#This one will help to app be on top of all other screns
+tk.wm_attributes("-topmost",1)
+
+#This is the interface of the game
+canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+present_color1 = "white"
+present_color2 = "black"
+presents_list = []
+presents_size = 55
+for i in range(presents_size):
+    x = random.randrange(virtual_game_x)
+    y = random.randrange(virtual_game_y)
+    id1 = canvas.create_oval(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=present_color2)
+    id2 = canvas.create_oval(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=present_color1)    
+    presents_list.append([x, y, id1, id2])
+print(presents_list)
